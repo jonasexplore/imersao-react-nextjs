b7c1eca9deaf8dc376b07bba62a00b1d134c189e
Jonas Brito <jonasexplore@gmail.com>
2021-01-25 23:14:45 -0300

    aula01

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,39 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import db from "../db.json";
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
+* {
+  box-sizing: border-box;
 }
+body {
+  margin: 0;
+  padding: 0;
+  /* New styles */
+  display: flex;
+  flex-direction: column;
+  font-family: 'Lato', sans-serif;
+  // Deixa branco no começo
+  color: ${({ theme }) => theme.colors.contrastText};
+}
+html, body {
+  min-height: 100vh;
+}
+#__next {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+`;
+
+const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import db from "../db.json";
+import Widget from "../src/components/Widget";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
+import QuizBackground from "../src/components/QuizBackground";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Teste</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+            <p>Opa</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/FalLying/imersao-react-nextjs" />
+    </QuizBackground>
+  );
 }
