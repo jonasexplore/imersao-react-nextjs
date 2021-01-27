2cf15290f6a4da7249c0abbf49ba6c51b77ea409
Jonas Brito <jonasexplore@gmail.com>
2021-01-27 12:25:39 -0300

    Adicionando título global.

--- pages/_app.js
+++ pages/_app.js
@@ -33,6 +33,7 @@ export default function App({ Component, pageProps }) {
     <>
       <ThemeProvider theme={theme}>
         <Head>
+          <title>FetichesQuiz - Imersão React Nextjs</title>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
