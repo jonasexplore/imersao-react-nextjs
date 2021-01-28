a8219236a89afba322e2f651f0f3c0a68d6462d7
Jonas Brito <jonasexplore@gmail.com>
2021-01-28 12:46:32 -0300

    Finalizando cálculo das questões e tela de resultados.

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,61 +1,26 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import { useRouter } from "next/router";
 import QuizBackground from "../src/components/QuizBackground";
 import Widget from "../src/components/Widget";
+import WidgetLoader from "../src/components/WidgetLoader";
 import db from "../db.json";
 import Button from "../src/components/Button";
+import Option from "../src/components/Option";
 import QuizLogo from "../src/components/QuizLogo";
 import Footer from "../src/components/Footer";
 import QuizContainer from "../src/components/QuizContainer";
-import ContentLoader from "react-content-loader";
+import AlternativesForm from "../src/components/AlternativesForm";
 import Confetti from "react-confetti";
+import styled from "styled-components";
 
 // Trabalhando nessa parte ainda 😕
 // export const Img = styled.img`
 //   width: 100%;
 //   max-height: 5rem;
 // `;
-
-export const Loader = (props) => (
-  <Widget>
-    <Widget.Header />
-    <ContentLoader
-      speed={2}
-      width={400}
-      height={360}
-      viewBox="0 0 400 360"
-      backgroundColor="rgba(40,40,40,0.2)"
-      foregroundColor="rgba(60,60,60, 0.2)"
-      {...props}
-    >
-      <rect x="24" y="71" rx="2" ry="2" width="306" height="13" />
-      <rect x="24" y="28" rx="0" ry="0" width="308" height="39" />
-      <rect x="23" y="92" rx="0" ry="0" width="167" height="13" />
-      <rect x="41" y="120" rx="0" ry="0" width="269" height="44" />
-      <rect x="42" y="171" rx="0" ry="0" width="268" height="42" />
-      <rect x="43" y="286" rx="0" ry="0" width="267" height="44" />
-      <rect x="43" y="221" rx="0" ry="0" width="268" height="42" />
-    </ContentLoader>
-  </Widget>
-);
-
-export const Option = styled.div`
-  outline: 0;
-  display: block;
-  width: 100%;
-  margin: 2% 0;
-  font-size: 0.9rem;
-  border-radius: 0.25rem;
-  padding: 0.7rem;
-  background-color: ${db.theme.colors.primary};
-  opacity: 0.6;
-  transition: background-color 0.5s;
-  &:hover {
-    background-color: ${db.theme.colors.secondary};
-    opacity: 1;
-    cursor: pointer;
-    transition: background-color 0.5s;
-  }
+const Tag = styled.span`
+  font-weight: bold;
+  color: ${(props) => props.color};
 `;
 
 export const QuestionWidget = ({
@@ -63,9 +28,13 @@ export const QuestionWidget = ({
   questionIndex,
   question,
   onSubmit,
+  addResult,
 }) => {
   const radioName = `radio_${questionIndex}`;
-
+  const [selectedAlternative, setSelectedAlternative] = useState(undefined);
+  const [isQuestionSubmited, setIsQuestionSubmited] = useState(false);
+  const isCorrect = selectedAlternative === question.answer;
+  const hasSelectedAlternative = selectedAlternative !== undefined;
   return (
     <Widget>
       <Widget.Header>
@@ -75,39 +44,97 @@ export const QuestionWidget = ({
       <Widget.Content>
         <h2>{question.title}</h2>
         <p>Selecione a alternativa que mais faz sentido para você:</p>
-        <form
+        <AlternativesForm
           onSubmit={(e) => {
             e.preventDefault();
-            onSubmit();
+            setIsQuestionSubmited(true);
+            setTimeout(() => {
+              addResult(isCorrect);
+              onSubmit();
+              setIsQuestionSubmited(false);
+              setSelectedAlternative(undefined);
+            }, 2 * 1000);
           }}
         >
           {question.alternatives.map((item, i) => {
+            const alternativeStatus = isCorrect ? "SUCCESS" : "ERROR";
+            const isSelected = selectedAlternative === i;
             return (
-              <Option as="label" htmlFor={i} key={i}>
-                <input name={radioName} id={i} type="radio" />
+              <Option
+                as="label"
+                htmlFor={i}
+                key={i}
+                data-selected={isSelected}
+                data-status={isQuestionSubmited && alternativeStatus}
+              >
+                <input
+                  style={{ display: "none" }}
+                  name={radioName}
+                  id={i}
+                  type="radio"
+                  checked={false}
+                  onChange={() => setSelectedAlternative(i)}
+                />
                 {` ${item}`}
               </Option>
             );
           })}
-
-          <Button type="submit">CONFIRMAR</Button>
-        </form>
+          <Button type="submit" disabled={!hasSelectedAlternative}>
+            CONFIRMAR
+          </Button>
+        </AlternativesForm>
       </Widget.Content>
     </Widget>
   );
 };
 
-export const Result = () => {
+export const Result = ({ results }) => {
   const width = window.innerWidth;
   const height = window.innerHeight;
+  const router = useRouter();
+  const { name } = router.query;
+  const [stats, setStats] = useState({ nivel: "", color: "white" });
+  const totalQuestionsResponse = results.length;
+  const totalAcceptedQuestions = results.filter((x) => x).length;
+  const aproveitamento = (
+    (totalAcceptedQuestions * 100) /
+    totalQuestionsResponse
+  ).toFixed(2);
+
+  useEffect(() => {
+    if (aproveitamento <= 30) setStats({ nivel: "Júnior", color: "white" });
+    else if (aproveitamento <= 50) setStats({ nivel: "Pleno", color: "green" });
+    else if (aproveitamento <= 80) setStats({ nivel: "Sênior", color: "red" });
+    else setStats({ nivel: "God", color: "yellow" });
+  }, []);
+
   return (
     <>
       <Confetti width={width} height={height} recycle={false} />
       <Widget>
         <Widget.Header>Resultado</Widget.Header>
         <Widget.Content>
-          Você chegou até o final! Mais ainda não conseguimos dizer se você é
-          bom o bastante. 😅
+          <p>
+            Você chegou até o final {`${name}`} 🎉! E acertou um total de{" "}
+            {totalAcceptedQuestions}{" "}
+            {totalAcceptedQuestions === 1 ? "questão" : "questões"} das{" "}
+            {totalQuestionsResponse} respondidas.
+          </p>
+          <p>Você obteve um aproveitamento de {aproveitamento}%.</p>
+          <p>
+            Calculamos seu aproveitamento com base em estudos avançados e
+            cruzamento de dados usando IA e declaramos você um programador nível{" "}
+            <Tag color={stats.color}>{stats.nivel}</Tag>.
+          </p>
+          <hr />
+          Detalhamento:
+          <ul>
+            {results.map((item, i) => (
+              <li key={i}>
+                {i + 1} - {item ? "Acertou" : "Errou"}
+              </li>
+            ))}
+          </ul>
         </Widget.Content>
       </Widget>
     </>
@@ -122,10 +149,16 @@ const screenStates = {
 
 export default function quiz() {
   const [screenState, setScreenState] = useState(screenStates.LOADING);
-  const totalQuestions = db.questions.length;
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [results, setResults] = useState([]);
+  const totalQuestions = db.questions.length;
+
   const questionIndex = currentQuestion;
   const question = db.questions[questionIndex];
+
+  function addResult(result) {
+    setResults([...results, result]);
+  }
 
   useEffect(() => {
     setTimeout(() => {
@@ -152,10 +185,11 @@ export default function quiz() {
             questionIndex={questionIndex}
             totalQuestions={totalQuestions}
             onSubmit={handleSubmit}
+            addResult={addResult}
           />
         )}
-        {screenState === screenStates.LOADING && <Loader />}
-        {screenState === screenStates.RESULT && <Result />}
+        {screenState === screenStates.LOADING && <WidgetLoader />}
+        {screenState === screenStates.RESULT && <Result results={results} />}
 
         <Footer />
       </QuizContainer>
