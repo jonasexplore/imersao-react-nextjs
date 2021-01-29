75ddbbcc7a302acb044506c3867df292328bd85e
Jonas Brito <jonasexplore@gmail.com>
2021-01-29 15:16:39 -0300

    Adicionando componente de link (SPA).

---
+++ src/components/Link/index.js
@@ -0,0 +1,10 @@
+import react from "react";
+import NextLink from "next/link";
+
+export default function Link({ children, href, ...props }) {
+  return (
+    <NextLink href={href} passHref>
+      <a {...props}>{children}</a>
+    </NextLink>
+  );
+}
